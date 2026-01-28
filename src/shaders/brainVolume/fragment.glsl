ad17b245269533ac051a2b118e5b7de0a32b4265
H0rseradish <sjhartill63@gmail.com>
2026-01-28 16:32:17 +0000

    continue tidying (decoupling?) of the raycasting function in thre fragment shader

--- src/shaders/brainVolume/fragment.glsl
+++ src/shaders/brainVolume/fragment.glsl
@@ -4,7 +4,6 @@ precision highp float;
 precision mediump sampler3D;
 
 uniform vec3 uVolumeSize;
-// uniform int u_renderstyle;
 uniform float uIsoSurfaceThreshold;
 uniform vec2 uColorMapValueRange;
 
@@ -89,20 +88,19 @@ void main() {
     );
 
     // So can now find the starting position on the front surface
-    //RENAME to rayEntryPosition? front on its own doesnt mean much?
     vec3 rayEntryPosition = vPosition + viewRayDirection * rayEntryDistance;
 
-    // Decide how many steps to take
+    // Determine how many steps to take:
     int stepCount = int(- rayEntryDistance / RELATIVE_STEP_SIZE + 0.5);
     if ( stepCount < 1 )
             discard;
 
-    // Get starting location and step vector in texture coordinates
+    // Get step vector for raycasting, and  the starting location in texture coordinates:
     vec3 rayStep = ((vPosition - rayEntryPosition) / uVolumeSize) / float(stepCount);
     vec3 rayStartVolumeCoords = rayEntryPosition / uVolumeSize;
 
-    // For testing: show the number of steps. This helps to establish
-    // whether the rays are correctly oriented
+    // "For testing: show the number of steps. This helps to establish
+    // whether the rays are correctly oriented"
     //'gl_FragColor = vec4(0.0, float(stepCount) / 1.0 / uVolumeSize.x, 1.0, 1.0);
     //'return;
 
@@ -128,7 +126,7 @@ void main() {
     // apply COLORMAP
 
     // add LIGHTING
-    // gl_FragColor = addLighting(val, refineVolumeCoords, dstep, viewRayDirection);
+    // gl_FragColor = addLighting(val, refineVolumeCoords, gradientStep, viewRayDirection);
 
     // FINAL COLOR
     if (gl_FragColor.a < 0.05)
@@ -155,43 +153,67 @@ void main() {
 //so raycasting finds a hit, shading uses the hit 
 //so this function should just be about finding the hits?
 //so it actually just determines  whether each fragment is a surface (and currently colors and lights it if it is!!!!!! ) So it needs to be a boolean and then if its true can do FragColor addLighting! AND IT NEEDS TO RETURN DATA on the hit.
-void raycastIsoSurface(vec3 rayStartVolumeCoords, vec3 rayStep, int stepCount, vec3 viewRayDirection) 
+void raycastIsoSurface(
+    vec3 rayStartVolumeCoords, 
+    vec3 rayStep, 
+    int stepCount, 
+    vec3 viewRayDirection
+    //outs: so can return more than one thing...
+    out bool hit,
+    out float hitScalarValue,
+    out vec3 hitVolumeCoords
+) 
 {
     gl_FragColor = vec4(0.0);	// init transparent
-    vec4 color3 = vec4(0.0);	// final color
-    vec3 dstep = 1.5 / uVolumeSize;	// step to sample derivative
+    // vec4 color3 = vec4(0.0); // final color - this isnt doing anything - maybe this was originally intended to be used instead of going directly to FragColor...
+
+    bool hit = false; // init as false
+
+    // gradientStep defines how far apart the samples are when estimating the gradient (the normal(!)) its the step between each compute of the gradient. The value 1.5 is a compromise for the smoothing between noise/artefacts and loss of detail. Should I call it normalSampleStep? or is that a step too far!
+    vec3 gradientStep = 1.5 / uVolumeSize;
+
+    //the current 'point' (obviously)
     vec3 currentVolumeCoords = rayStartVolumeCoords;
 
-    //what is this 0.02 ????
-    float low_threshold = uIsoSurfaceThreshold - 0.02 * (uColorMapValueRange[1] - uColorMapValueRange[0]);
+    //what is this 0.02 ???? It is to "Lower threshold to detect crossings before exact iso value" ?? still do not fully understand.
+    float isoSurfaceSearchThreshold = uIsoSurfaceThreshold - 0.02 * (uColorMapValueRange[1] - uColorMapValueRange[0]);
 
-    // Enter the raycasting loop. 
-    // NB In WebGL 1 the loop index cannot be compared with a non-constant expression. So we use a hard-coded max, and an additional condition inside the loop.
+
+    // Enter the raycasting loop:
+
+    // NB "In WebGL 1 the loop index cannot be compared with a non-constant expression. So we use a hard-coded max, and an additional condition inside the loop".
     for (int iter = 0; iter < MAX_STEPS; iter++) {
         if (iter >= stepCount)
             break;
-        // the volume is a scalar field, ie, at every point thesere is a single number stored (representing intensity, or density)
+        // the volume is a 'scalar field', ie, at every point there is a single number stored (representing intensity, or density)
         // Sample from the 3D texture to get the scalarValue:
         float scalarValue = sampleVolume(currentVolumeCoords);
 
-        if (scalarValue > low_threshold) {
-            // Take the last interval in smaller steps
+        if (scalarValue > isoSurfaceSearchThreshold) {
+            // "...Take the last interval in smaller steps (refine the steps!)"
             vec3 refineVolumeCoords = currentVolumeCoords - 0.5 * rayStep;
             vec3 refineStep = rayStep / float(REFINEMENT_STEPS);
+
             for (int i = 0; i < REFINEMENT_STEPS; i++) {
                 scalarValue = sampleVolume(refineVolumeCoords);
-                
 
                 if (scalarValue > uIsoSurfaceThreshold) {
-                    //so... this addLighting needs to happen in main, so this needs to just return true - wait, the parameters come from from this function. - and I can only return one thing from a glsl function??
+
+                    //so... outs defined here
+                    hit = true;
+                    hitScalarValue = scalarValue;
+                    hitVolumeCoords = refineVolumeCoords;
+
+                    // addLighting() needs to happen in main, so this needs to just return true - wait, the parameters come from from this function. - and I can only return one thing from a glsl function??
                     //this is where 'out' comes in.... 
-                    gl_FragColor = addLighting(scalarValue, refineVolumeCoords, dstep, viewRayDirection);
+                    gl_FragColor = addLighting(hitScalarValue, hitVolumeCoords, gradientStep, viewRayDirection);
                     return;
                 }
+                // take a refined step:
                 refineVolumeCoords += refineStep;
             }
         }
-        // Advance location (currentVolumeCoords) deeper into the volume
+        // Advance the location (currentVolumeCoords) deeper into the volume, or in other words, Step!
         currentVolumeCoords += rayStep;
     }
 }
